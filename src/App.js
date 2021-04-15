b41f6e3605aa2bc7ce1f48136635a45bac17f5c1
unknown <hubert.bonney49@gmail.com>
2021-04-15 21:23:39 +0000

    initialize

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import './App.css';
+import React from 'react';
+import Userform from './Components/Userform';
+import UserItem from './components/UserItem';
+import UserList from './components/UserList';
+
+class App extends React.Component{
+  constructor(props){
+    super(props)
+    this.state = {
+      users : [] //setting up an empty array here so that the users can be updated here
+
+    }
+  }
+
+  //a function that will take the list of users
+  addUser = (user) => {
+    this.setState({
+      users :[...this.state.users, user] //copying each user into the array
+    })
+  }
+
+  render(){
+    return (
+      <div className="row">
+        <div className = "col-md-6 userinput">
+          <UserForm addUser = {this.addUser} />
+        </div>
+        
+        <div className = "col-md-6 userlist">
+          <UserList users = {this.state.users}/>   
+        </div>
+        
+      </div>
+  
+    );
+  }
+  
+}
+
+export default App;
+
---
+++ src/Components/UserList.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import UserItem from './UserItem';
+
+class UserList extends React.Component{
+    render(){
+        return (
+            <div>
+                <h2>User List</h2>
+                {this.props.users.map((user) => {
+                    return <UserItem  user = {user}/>;
+                })}
+            
+            </div>
+        )
+    }
+}
+
+export default UserList;
+
